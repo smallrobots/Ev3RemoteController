fc2733b5bfe0afe5b59ff7b3a8a5b4826eea5668
Oreste Riccardo Natale <o.r.natale@mosaico-mi.it>
2018-02-02 21:36:32 +0100

    General fix

---
+++ Ev3RemoteController/Models/Ev3UDPServer.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using System;
+using Windows.Networking.Sockets;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+using Newtonsoft.Json;
+using Windows.Networking;
+
+namespace Smallrobots.Ev3RemoteController.Models
+{
+    public class Ev3UDPServer : Model
+    {
+        #region Fields
+        /// <summary>
+        /// Host to contact
+        /// </summary>
+        IPAddress hostIpAddres;
+
+        /// <summary>
+        /// Host port of service to contact
+        /// </summary>
+        int remoteHostPort;
+
+        /// <summary>
+        /// This controller Ip address
+        /// </summary>
+        IPAddress controllerIpAddress;
+
+        /// <summary>
+        /// This controller Ip port
+        /// </summary>
+        int controllerIpPort;
+        #endregion
+
+        #region Properties
+        Ev3RobotModel robotModel;
+        public Ev3RobotModel RobotModel
+        {
+            get => robotModel;
+            set
+            {
+                robotModel = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+        #region Constructor
+
+        #endregion
+    }
+}
